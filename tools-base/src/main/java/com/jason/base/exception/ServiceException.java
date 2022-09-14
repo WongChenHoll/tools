a43a35f9ec04f1ec54162c3504feda9c8c4b3489
chenghaojason <whx20100101@163.com>
2022-09-14 15:15:35 +0800

    1

--- tools-base/src/main/java/com/jason/base/exception/ServiceException.java
+++ tools-base/src/main/java/com/jason/base/exception/ServiceException.java
@@ -1,16 +1,39 @@
 package com.jason.base.exception;
 
+import com.jason.base.enums.ResponseEnum;
+
 /**
  * @author WangChenHol
  * @date 2021-9-26 12:41
  **/
-public class ServiceException extends Exception{
+public class ServiceException extends Exception {
 
-    public ServiceException(){
+    private final String code;
+
+    private ServiceException() {
         super();
+        this.code = null;
     }
 
-    public ServiceException(String message){
+    private ServiceException(String message) {
         super(message);
+        this.code = null;
+    }
+
+    private ServiceException(String message, String code) {
+        super(message);
+        this.code = code;
+    }
+
+    public static ServiceException baseException(String message, ResponseEnum responseEnum) {
+        return new ServiceException(message, responseEnum.getCode());
+    }
+
+    public static ServiceException paramsException(String message) {
+        return baseException(message, ResponseEnum.PARAM_VALID_ERROR);
+    }
+
+    public static ServiceException serverException(String message) {
+        return baseException(message, ResponseEnum.SERVER_ERROR);
     }
 }
