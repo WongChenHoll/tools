c6e8c656bf96330769ccccfcea7486e5de0d685e
chenghaojason <whx20100101@163.com>
2023-12-22 13:49:05 +0800

    fix

--- tools-base/src/main/java/com/jason/base/enums/ResponseEnum.java
+++ tools-base/src/main/java/com/jason/base/enums/ResponseEnum.java
@@ -9,14 +9,18 @@ public enum ResponseEnum {
     ERROR("40000", "请求失败"),
 
     VALID_PARAM_ERROR("40100", "参数校验失败"),
-    VALID_ERROR("40100", "内容校验失败"),
+    VALID_ERROR("40101", "内容校验失败"),
 
     FILE_ERROR("40200", "文件错误"),
     FILE_NOT_EXIST("40201", "文件不存在"),
     FILE_PATH_ERROR("40202", "文件路径错误"),
     PATH_ERROR("40210", "路径错误"),
 
-    CONNECTION_FAIL("40900", "参获取连接失败"),
+    USER_AUTH_ERROR("40500","认证失败，请联系管理员"),
+    JURISDICTION_ERROR("40501","您无权操作，请联系管理员添加"),
+
+    CONNECTION_FAIL("40900", "获取连接失败"),
+
     SERVER_ERROR("50000", "服务器异常");
 
     private final String code;
@@ -31,8 +35,6 @@ public enum ResponseEnum {
     }
 
     ResponseEnum(String code, String message) {
-
-
         this.code = code;
         this.message = message;
     }
